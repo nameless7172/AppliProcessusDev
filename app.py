515793a0a453eadfded6d8449b66dca5059b765d
nameless7172 <mikheil17@gmail.com>
2023-03-16 11:57:15 +0100

    added datatable for training programs

--- app.py
+++ app.py
@@ -4,9 +4,26 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+import json
 
 app = Flask(__name__)
 cors = CORS(app)
+
+
+# Set up authentication using the JSON client file
+creds = service_account.Credentials.from_service_account_file(
+    'client_secret.json',
+    scopes=['https://www.googleapis.com/auth/spreadsheets.readonly']
+)
+# Define the spreadsheet ID and range
+SPREADSHEET_ID = '1sokVn-TX3t1DQ3IQPAAl7B-F_xnyZfLYShrJ_MxEfGY'
+RANGE_NAME = 'Sheet1!A1:ZZ'  # Include all columns and rows
+
+# Fetch the data from the Google Sheets API
+service = build('sheets', 'v4', credentials=creds)
+result = service.spreadsheets().values().get(
+    spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+values = result.get('values', [])
 
 @app.route("/")
 def home():
@@ -19,30 +36,29 @@ def page1():
 def page2():
     return render_template("page2.html")
 
-# Set up authentication using the JSON client file
-creds = service_account.Credentials.from_service_account_file(
-    'client_secret.json',
-    scopes=['https://www.googleapis.com/auth/spreadsheets.readonly']
-)
 
-# Define a route for the GET request
 @app.route('/google-sheet-content', methods=['GET'])
 def get_google_sheet_content():
-    # Define the spreadsheet ID and range
-    spreadsheet_id = '1sokVn-TX3t1DQ3IQPAAl7B-F_xnyZfLYShrJ_MxEfGY'
-    range_name = 'Sheet1!A1:B2'  # Replace with the range you want to download
-
-    # Fetch the data from the Google Sheets API
-    service = build('sheets', 'v4', credentials=creds)
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheet_id, range=range_name).execute()
-    values = result.get('values', [])
-
-    # Convert the data to JSON and return it
+    # Check if data was retrieved successfully
     if not values:
         return jsonify({'error': 'No data found'})
-    else:
-        return jsonify({'data': values})
+
+    # Render the template with the data as a context variable
+    return render_template('datatable.html', data=values)
+
+@app.route('/render_data_cell/<int:row_index>', methods=['POST'])
+def render_data_cell(row_index):
+    # Get the row of data corresponding to the clicked element
+    row_data = values[row_index]
+
+    # Render the data_cell.html template with the row data as a context variable
+    rendered_data_cell = render_template('data_cell.html', data=row_data)
+
+    # Render the program.html template with the rendered data_cell.html as a context variable
+    return render_template('program.html', data=row_data, rendered_data_cell=rendered_data_cell)
+
+
+
 
 if __name__ == "__main__":
     app.run()
