d2c2b55c7530874caf33766b93e44f52376be812
Mikhail Kuchukhidze <mikhail.kuchukhidze@isento-ecommerce.de>
2023-03-17 18:47:59 +0100

    implementation of modules and exercises

--- app.py
+++ app.py
@@ -17,12 +17,11 @@ creds = service_account.Credentials.from_service_account_file(
 )
 # Define the spreadsheet ID and range
 SPREADSHEET_ID = '1sokVn-TX3t1DQ3IQPAAl7B-F_xnyZfLYShrJ_MxEfGY'
-RANGE_NAME = 'Programme 1!A1:ZZ'  # Include all columns and rows
+RANGE_NAME = 'General!A2:ZZ'  # Include all columns and rows
 
 # Fetch the data from the Google Sheets API
 service = build('sheets', 'v4', credentials=creds)
-result = service.spreadsheets().values().get(
-    spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
 values = result.get('values', [])\
 
 # Check if data was retrieved successfully
@@ -60,14 +59,53 @@ def get_google_sheet_content():
 
 @app.route('/render_data_cell/<int:row_index>', methods=['POST'])
 def render_data_cell(row_index):
-    # Get the row of data corresponding to the clicked element
-    row_data = values[row_index]
+    print(values[row_index][0])
+    
+    # Define the spreadsheet ID and range
+    SPREADSHEET_ID = '1sokVn-TX3t1DQ3IQPAAl7B-F_xnyZfLYShrJ_MxEfGY'
+    RANGE_NAME = values[row_index][0] + '!A2:ZZ'  # Include all columns and rows
+
+    # Fetch the data from the Google Sheets API
+    service = build('sheets', 'v4', credentials=creds)
+    result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+    new_values = result.get('values', [])\
+    
+    # print(new_values)
+    seen_strings = {}
+    new_list = []
+
+    for item in new_values:
+        if item[0] not in seen_strings:
+            seen_strings[item[0]] = True
+            new_list.append(item)
 
     # Render the data_cell.html template with the row data as a context variable
-    rendered_data_cell = render_template('data_cell.html', data=row_data)
+    rendered_data_cell = render_template('data_cell.html', data=new_list)
 
     # Render the program.html template with the rendered data_cell.html as a context variable
-    return render_template('program.html', data=row_data, rendered_data_cell=rendered_data_cell)
+    return render_template('program.html', data=new_list, rendered_data_cell=rendered_data_cell)
+
+#TODO 
+@app.route('/render_sub_data_cell/<int:row_index>', methods=['POST'])
+def render_sub_data_cell(row_index):
+    print(values[row_index][0])
+    
+    # Define the spreadsheet ID and range
+    SPREADSHEET_ID = '1sokVn-TX3t1DQ3IQPAAl7B-F_xnyZfLYShrJ_MxEfGY'
+    RANGE_NAME = values[row_index][0] + '!A2:ZZ'  # Include all columns and rows
+
+    # Fetch the data from the Google Sheets API
+    service = build('sheets', 'v4', credentials=creds)
+    result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+    new_values = result.get('values', [])\
+    
+    # print(new_values)
+
+    # Render the data_cell.html template with the row data as a context variable
+    rendered_data_cell = render_template('sub_data_cell.html', data=new_values)
+
+    # Render the program.html template with the rendered sub_data_cell.html as a context variable
+    return render_template('module.html', data=new_values, rendered_data_cell=rendered_data_cell)
 
 
 
